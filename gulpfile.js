fb3fb65ba9de82ec55663eed50230050550fd8af
Kevin Nguyen <kevin.nguyen@lineophile.com>
2017-01-06 14:15:33 -0800

    Site setup with gulp and livereload

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    connect = require('gulp-connect');
+
+gulp.task('webserver', () => {
+    connect.server({
+        root: 'build',
+        livereload: true,
+        port: 9002
+    });
+});
+
+gulp.task('html', () => {
+    return gulp.src('./src/index.html')
+        .pipe(gulp.dest('./build/'))
+});
+
+gulp.task('js', () => {
+    return gulp.src('./src/js/*.js')
+        .pipe(gulp.dest('./build/js/'))
+});
+
+gulp.task('css', () => {
+    return gulp.src('./src/css/*.css')
+        .pipe(gulp.dest('./build/css/'))
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./src/index.html', ['html']);
+    gulp.watch('./src/js/*.js', ['js']);
+    gulp.watch('./src/css/*.css', ['css']);
+})
+
+gulp.task('reload', () => {
+        watch(['build/index.html', 'build/js/*.js', 'build/css/*.css'])
+        .pipe(connect.reload());
+})
+
+gulp.task('default', ['webserver', 'html', 'js', 'css', 'watch', 'reload']);
